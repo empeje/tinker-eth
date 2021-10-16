7e8d550642742d0d95f116443947db927cbcc3eb
Abdurrachman Mappuji <abdurrachman.mappuji@mekari.com>
2021-10-16 15:06:43 +0700

    Learn mappings

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -8,13 +8,29 @@ contract YourContract {
 
   //event SetPurpose(address sender, string purpose);
 
-  string public purpose = "Building Unstoppable Apps!!!";
+  string public purpose = "Building Unstoppable Apps!!! - jack";
+
+  mapping(address => uint) public balance;
+  address public owner = 0x20B35d6E0C4accf0391FD8fA98bBf63d737fC8AA;
+
 
   constructor() {
     // what should we do on deploy?
+      balance[owner] = 100;
   }
 
-  function setPurpose(string memory newPurpose) public {
+  function transfer(address to, uint256 amount) public {
+      require( amount <= balance[msg.sender], "NOT ENOUGH!!!!");
+      balance[msg.sender] -= amount;
+      balance[to]+= amount;
+  }
+
+
+  function setPurpose(string memory newPurpose) public payable {
+//      require(msg.sender == owner, "NOT OWNER!");
+
+      require(msg.value == 0.01 ether, "NOT OWNER!");
+
       purpose = newPurpose;
       console.log(msg.sender,"set purpose to",purpose);
       //emit SetPurpose(msg.sender, purpose);
